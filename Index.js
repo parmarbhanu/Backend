e87c6fada3383305bf46d9d308f65511e76f52e7
unknown <parmarsinghbhanu@gmail.com>
2023-07-14 22:08:43 +0530

    only for persional refrence

---
+++ Index.js
@@ -0,0 +1,50 @@
+const express = require("express");              //add express 
+const app = express(); 
+const mongoose = require("mongoose");            //add mongoose
+const dotenv = require("dotenv");                // add dotenv
+const cookieParser = require("cookie-parser");   //add cookies parser
+const PORT = process.env.PORT || 5000;           //desfinr port
+const cors = require("cors");                    //add coros
+
+dotenv.config();
+app.use(express.json());
+
+app.use(cors({
+    origin: [
+       "http://localhost:3000",
+    ],
+     credentials: true,
+   })
+);
+app.use(cookieParser());
+
+
+
+
+// connect to mongoDB   this connect mongodb to a file named as .env
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MDB_CONNECT) 
+.then(()=>{console.log('Mongodb connected')})
+.catch((err)=>{console.log("Error in connection",err)});
+
+
+
+
+
+//  set up routes
+app.use("/Use",require("./router/adminRouter"));
+
+          
+
+
+//return the port were our server is going to listen
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+
+
+//tahe the html content of the website from the file named as index.html ans display it on the local host
+const path=require("path");
+app.use(express.static('client/build'));
+ app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client','build','index.html'));
+});
---
+++ Models/userModel.js
@@ -0,0 +1,19 @@
+// this file contain all the things that you required to validate your data bases
+
+
+
+// a user is created heare 
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username:  { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+});
+
+const User = mongoose.model('user', UserSchema);
+module.exports=User;
+
+
+
+// for any more field of validation jist visit the google to help
